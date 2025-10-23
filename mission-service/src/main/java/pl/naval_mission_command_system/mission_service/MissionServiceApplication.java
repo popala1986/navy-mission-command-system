d17fa2fb0409254d88f02ed95fd1764e79a14b37
Paweł <p.opala1986@gmail.com>
2025-10-23 14:54:05 +0200

    Initial commit: mission-service microservice

---
+++ mission-service/src/main/java/pl/naval_mission_command_system/mission_service/MissionServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.naval_mission_command_system.mission_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissionServiceApplication.class, args);
+	}
+
+}
---
+++ mission-service/src/test/java/pl/naval_mission_command_system/mission_service/MissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.naval_mission_command_system.mission_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
