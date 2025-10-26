3d03c2fd0157b8d36b74e77dcc045084432167b9
Paweł <p.opala1986@gmail.com>
2025-10-26 09:58:45 +0100

    docs: add JavaDoc documentation to MIssionService class and public method

--- mission-service/src/main/java/pl/naval_mission_command_system/mission_service/service/MissionService.java
+++ mission-service/src/main/java/pl/naval_mission_command_system/mission_service/service/MissionService.java
@@ -8,6 +8,9 @@ import pl.naval_mission_command_system.mission_service.repository.MissionReposit
 import java.util.List;
 import java.util.Random;
 
+/**
+ * Service class responsible for mission-related business operations
+ */
 @Service
 @RequiredArgsConstructor
 public class MissionService {
@@ -15,15 +18,30 @@ public class MissionService {
     private final MissionRepository missionRepository;
     private final Random random;
 
+    /**
+     * Retrieves all mission stored i the database
+     * @return a list of all {@link Mission} entities
+     * */
     public List<Mission> getAllMission() {
         return missionRepository.findAll();
     }
 
+    /**
+     * Returns a randomly selected mission from the database
+     * @return a list of all {@link Mission} entities
+     */
     public Mission getRandomMission() {
         List<Mission> allMission = missionRepository.findAll();
         return allMission.isEmpty() ? null : allMission.get(random.nextInt(allMission.size()));
     }
 
+    /**
+     * Persists a mission entity to the database.
+     * If the mission already exists, it will be updated.
+     *
+     * @param mission the {@link Mission} to save
+     * @return the saved {@link Mission} entity
+     */
     public Mission saveMission(Mission mission) {
         return missionRepository.save(mission);
     }
