228cb1d66093f932b26ed8fd1230e13028f9bb38
Paweł <p.opala1986@gmail.com>
2025-10-29 17:57:46 +0100

    feat: initialize ship-service microservice with base Spring Boot setup

---
+++ ship-service/src/main/java/pl/naval_mission_command_system/ship_service/ShipServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.naval_mission_command_system.ship_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipServiceApplication.class, args);
+	}
+
+}
