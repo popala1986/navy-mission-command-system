5b407d28848ed0a27be82966b8a4d35ddb0f3606
Paweł <p.opala1986@gmail.com>
2025-10-29 11:14:23 +0100

    feat(ship-service): add new microservice for naval ship management

---
+++ ship-service/src/test/java/pl/naval/mission_command_system/ship_service/ShipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.naval.mission_command_system.ship_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
